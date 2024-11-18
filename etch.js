e9741b109be4b4229684cf62afbd1c57b0d2a3fc
Reneil <123956661+reneilfuna@users.noreply.github.com>
2024-11-18 13:33:41 +0000

    Create 16 divs in js and append to container div

--- etch.js
+++ etch.js
@@ -1,2 +1,36 @@
 const container = document.getElementById("container");
 
+const div1 = document.createElement("div");
+const div2 = document.createElement("div");
+const div3 = document.createElement("div");
+const div4 = document.createElement("div");
+const div5 = document.createElement("div");
+const div6 = document.createElement("div");
+const div7 = document.createElement("div");
+const div8 = document.createElement("div");
+const div9 = document.createElement("div");
+const div10 = document.createElement("div");
+const div11 = document.createElement("div");
+const div12 = document.createElement("div");
+const div13 = document.createElement("div");
+const div14 = document.createElement("div");
+const div15 = document.createElement("div");
+const div16 = document.createElement("div");
+
+container.appendChild(div1);
+container.appendChild(div2);
+container.appendChild(div3);
+container.appendChild(div4);
+container.appendChild(div5);
+container.appendChild(div6);
+container.appendChild(div7);
+container.appendChild(div8);
+container.appendChild(div9);
+container.appendChild(div10);
+container.appendChild(div11);
+container.appendChild(div12);
+container.appendChild(div13);
+container.appendChild(div14);
+container.appendChild(div15);
+container.appendChild(div16);
+
