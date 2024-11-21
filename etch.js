c368e73471927ba87392cd1b84872c14b6f67f33
Reneil <123956661+reneilfuna@users.noreply.github.com>
2024-11-21 13:25:41 +0000

    Implement functional random color trail
    Mistook rgb() for a javascript function when it is a CSS function only.
    As a result, had to pass the rbg value through the function as a string,
    using .join() to reduce the amount of strings passed.

--- etch.js
+++ etch.js
@@ -37,7 +37,7 @@ function randomRGB(){
     let R = Math.round(Math.random() * 255);
     let G = Math.round(Math.random() * 255);
     let B = Math.round(Math.random() * 255);
-    return rgb(R, G, B);
+    return ["rgb(",R,",",G,",",B,")"].join("");
 }
 
 // Create 2 listener functions using event delegation
@@ -45,13 +45,14 @@ function randomRGB(){
 // Create 1 listener function for mouseover event
  container.addEventListener("mouseover", (event) => {
     let target = event.target;
-    if (colorBtn.textContent = "Colour Mode: Off"){
+    if (colorBtn.textContent == "Colour Mode: Off"){
         if (target.className == "gridElement")
             target.setAttribute("class", "overGridElement")
     }
     else if (colorBtn.textContent = "Colour Mode: On"){
         if (target.className == "gridElement"){
             target.setAttribute("class", "overColorElement")
+            console.log(randomRGB())
             target.style["background-color"] = randomRGB();
         }
     }
