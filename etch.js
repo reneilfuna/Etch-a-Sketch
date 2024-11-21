6759b417ce94342d2c3ab54241e6628086decc4e
Reneil <123956661+reneilfuna@users.noreply.github.com>
2024-11-21 13:15:08 +0000

    Build function to generate random RGB value

--- etch.js
+++ etch.js
@@ -32,7 +32,13 @@ colorBtn.addEventListener("click", (event) => {
     } 
 });
 
-
+function randomColorGen(){
+    // Generate 3 random values between 255
+    let R = Math.round(Math.random() * 255);
+    let G = Math.round(Math.random() * 255);
+    let B = Math.round(Math.random() * 255);
+    return rgb(R, G, B);
+}
 
 // Create 2 listener functions using event delegation
 
